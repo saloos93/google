1bd3c65fce5825d82d871f8204831a8d3a3436c6
milad-emami <milad.emami1025@gmail.com>
2022-04-29 16:42:37 +0430

    feat: add top header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,37 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import User from "./User";
 
 export const Header = () => {
-  return <div>Header</div>;
+  const router = useRouter();
+  return (
+    <header className="flex justify-between p-5 ">
+      <div className="flex space-x-4 items-center">
+        <Link href="https://about.google/">
+          <a className="link">About</a>
+        </Link>
+        <Link href="https://store.google.com/">
+          <a className="link">Store</a>
+        </Link>
+      </div>
+      <div className="flex space-x-4 items-center">
+        <Link href="https://mail.google.com">
+          <a className="link">Gmail</a>
+        </Link>
+
+        <a
+          onClick={() =>
+            router.push(
+              `/search?term=${router.query.term || "google"}&searchType=image`
+            )
+          }
+          className="link"
+        >
+          Images
+        </a>
+        <User />
+      </div>
+    </header>
+  );
 };
